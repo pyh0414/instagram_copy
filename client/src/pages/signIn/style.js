6dcafc7799b457bda3b0a7348199fc0c736e60f9
yeonho Park <whwlsvy12@naver.com>
2020-05-27 18:17:21 +0900

    style 파일로 분리

---
+++ client/src/pages/signIn/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { Form } from "antd";
+
+export const Wrapper = styled.div`
+	height: 70vh;
+	display: flex;
+`;
+
+export const CustomForm = styled(Form)`
+	margin: auto;
+	width: 22%;
+`;
---
+++ client/src/pages/signUp/style.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Form, Button } from "antd";
+
+export const Wrapper = styled.div`
+	height: 70vh;
+	display: flex;
+`;
+
+export const FormCustom = styled(Form)`
+	display: block;
+	margin: auto;
+	width: 22%;
+`;
+
+export const Foot = styled.div`
+	text-align: center;
+	margin-top: 20px;
+`;
+
+export const IdCheckButtonCustom = styled(Button)`
+	margin-top: 5px;
+`;
+
+export const ImageCustom = styled.img`
+	margin-top: 15px;
+	width: 200px;
+	height: 200px;
+`;
