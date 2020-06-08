a10f81a3454863da44e756244ea5cefeed7018ef
yeonho Park <whwlsvy12@naver.com>
2020-06-08 18:22:34 +0900

    add user query

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,7 +1,17 @@
-const Query = {
-	hello() {
-		return "!!!!";
+export const Query = {
+	async user(parent: any, args: any, { prisma }: any, info: any) {
+		if (args.query) {
+			const user = await prisma.query.user(
+				{
+					where: {
+						id: args.query,
+					},
+				},
+				info
+			);
+			return user;
+		} else {
+			throw new Error("id가 존재하지 않습니다.");
+		}
 	},
 };
-
-export default Query;
