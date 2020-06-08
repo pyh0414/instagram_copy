b12629de2d88717690f020ad1b35aec48b83cdd1
yeonho Park <whwlsvy12@naver.com>
2020-06-08 18:18:40 +0900

    add sigunUp validation

---
+++ client/src/utils/signUpValidation.js
@@ -0,0 +1,28 @@
+import { message } from "antd";
+
+export default ({
+	password,
+	profile,
+	passwordCheck,
+	hasSameIdChecked,
+	hasSameId,
+}) => {
+	if (password !== passwordCheck) {
+		message.error("비밀번호가 서로 다릅니다.", 0.5);
+		return false;
+	}
+
+	if (profile.trim() === "") {
+		message.error("프로필을 입력해 주세요.", 0.5);
+		return false;
+	}
+	if (!hasSameIdChecked) {
+		message.error("아이디 중복확인을 해주세요", 0.5);
+		return false;
+	}
+	if (hasSameId) {
+		message.error("중복된 아이디 입니다", 0.5);
+		return false;
+	}
+	return true;
+};
