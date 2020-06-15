f267e02d7e2b885fa2b8d6795048126e9a71bc23
yeonho Park <whwlsvy12@naver.com>
2020-06-15 15:10:29 +0900

    remove Promise<..> return

--- server/src/modules/User/resolver.ts
+++ server/src/modules/User/resolver.ts
@@ -1,11 +1,14 @@
 import { Resolver, Query, Mutation, Arg, Ctx } from "type-graphql";
-import { User, createUserInput, CTX } from "./type";
+import bcrypt from "bcryptjs";
+
+import { User, createUserInput, signInInput, CTX, AuthPayload } from "./type";
 import getHashedPassword from "../../utils/getHashedPassword";
+import generateToken from "../../utils/generateToken";
 
 @Resolver()
 export class UserResolver {
-	@Query(() => User, { nullable: true })
-	async user(@Arg("id") id: string, @Ctx() ctx: CTX): Promise<User | null> {
+	@Query((returns) => User, { nullable: true })
+	async user(@Arg("id") id: string, @Ctx() ctx: CTX) {
 		const { prisma } = ctx;
 		if (id) {
 			const user = await prisma.query.user({
@@ -19,11 +22,35 @@ export class UserResolver {
 		}
 	}
 
-	@Mutation(() => Boolean)
-	async createUser(
-		@Arg("user") user: createUserInput,
-		@Ctx() ctx: CTX
-	): Promise<Boolean> {
+	@Query((returns) => AuthPayload)
+	async signIn(@Arg("user") user: signInInput, @Ctx() ctx: CTX) {
+		const { prisma } = ctx;
+		const { id, password } = user;
+
+		const fullUser = await prisma.query.user({
+			where: {
+				id,
+			},
+		});
+		console.log(fullUser);
+		if (!fullUser) {
+			throw new Error("가입정보를 다시 확인해 주세요");
+		}
+		const isPasswordSame = bcrypt.compare(password, fullUser.password);
+
+		if (!isPasswordSame) {
+			throw new Error("가입정보를 다시 확인해 주세요");
+		}
+		const Token = generateToken(id);
+
+		return {
+			user: fullUser,
+			token: Token,
+		};
+	}
+
+	@Mutation((returns) => Boolean)
+	async createUser(@Arg("user") user: createUserInput, @Ctx() ctx: CTX) {
 		const { id, name, password, profile } = user;
 		const { prisma } = ctx;
 
