17b18da6cfef7ef2101b0c7299dd471683b552d9
yeonho Park <whwlsvy12@naver.com>
2020-06-17 02:38:20 +0900

    add IS_LOGGED_IN query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,30 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import { Router } from "@reach/router";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
 import Pages from "./pages";
 import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 
+const IS_LOGGED_IN = gql`
+	query {
+		user @client {
+			id
+			name
+			password
+			profile
+		}
+	}
+`;
+
 const App = () => {
-	const [loginStatus, setLoginStatus] = useState(false);
-
-	const onLoginStatus = useCallback(() => {
-		setLoginStatus(true);
-	}, [loginStatus]);
-
-	return loginStatus ? (
+	const { data } = useQuery(IS_LOGGED_IN);
+	return data && data.user ? (
 		<Pages />
 	) : (
 		<Router>
-			<SignIn onLoginStatus={onLoginStatus} path="/" />
+			<SignIn path="/" />
 			<SignUp path="signUp" />
 		</Router>
 	);
