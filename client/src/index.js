5c4331c8bde1c54eb6a38d156cbb63a9fa9ecc06
yeonho Park <whwlsvy12@naver.com>
2020-06-17 02:47:35 +0900

    init user

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,28 @@ import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
-
 import { createUploadLink } from "apollo-upload-client";
+
+import userResolver from "./resolvers/user";
+import typeDefs from "./types";
 
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less' // antd를 사용하기 위함
 import App from "./App";
 
+const cache = new InMemoryCache();
+
 const client = new ApolloClient({
-	cache: new InMemoryCache(),
+	cache,
 	link: createUploadLink({ uri: "http://localhost:4000/graphql" }),
+	resolvers: [userResolver],
+	typeDefs,
 	connectToDevTools: true,
+});
+
+cache.writeData({
+	data: {
+		user: null,
+	},
 });
 
 ReactDOM.render(
