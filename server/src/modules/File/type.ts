910ec21e4f6d1a21a31a91c5bc7f464d9152537f
yeonho Park <whwlsvy12@naver.com>
2020-06-23 15:29:52 +0900

    modify type

--- server/src/modules/File/type.ts
+++ server/src/modules/File/type.ts
@@ -1,8 +1,35 @@
 import { Stream } from "stream";
+import { ObjectType, Field, InputType } from "type-graphql";
 
-export interface UPLOAD {
+export type SINGLE_FILE_UPLOAD = {
 	filename: string;
-	mimetype: string;
-	encoding: string;
 	createReadStream: () => Stream;
+};
+
+// export type MULTIPLE_FILE_UPLOAD = (
+// 	map: any,
+// 	filename: string,
+// 	createReadStream: () => Stream
+// ) => Array<[string, any]>;
+
+// type Image {
+// 	id: ID! @id @default(autoincrement())
+// 	src: String!
+// 	createdAt: DateTime! @createdAt
+// 	post: Post! @relation(link: INLINE, onDelete: CASCADE)
+// }
+
+@ObjectType()
+export class File {
+	@Field()
+	id: number;
+
+	@Field()
+	src: string;
+
+	@Field()
+	createAt: Date;
+
+	@Field()
+	postId: number;
 }
--- server/src/modules/User/type.ts
+++ server/src/modules/User/type.ts
@@ -3,20 +3,47 @@ import { ObjectType, Field, InputType } from "type-graphql";
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 
+import { Post } from "../Post/type";
 @ObjectType()
 export class User {
-	@Field({ nullable: false })
-	userId: string;
+	@Field()
+	id: number;
 
-	@Field({ nullable: false })
-	userPw: string;
-
-	@Field({ nullable: false })
+	@Field()
 	name: string;
 
-	@Field({ nullable: false })
+	@Field()
+	userId: string;
+
+	@Field()
+	userPw: string;
+
+	@Field()
 	profile: string;
+
+	@Field()
+	createAt: Date;
+
+	@Field((type) => [Post], { nullable: true }) // generic types(promise, arrays)에 대해서는 따로 명시해 주어여함
+	posts?: Post[]; // posts는 user를 생성할 때 없을 수 있기 때문에 nullable: true와 ?를 붙여줘야함
+
+	@Field((type) => [User], { nullable: true }) // 유저를 생성할 때 follower가 없다
+	follower?: User[];
 }
+
+// id: ID! @id
+// name: String!
+// userId: String @unique
+// userPw: String!
+// profile: String!
+// createdAt: DateTime! @createdAt
+// # posts: [Post!]!
+// # user: @relation(name: "LikeToPost", onDelete: CASCADE)
+// # comments: [Comment!]!
+// # chats: [Chat!]!
+// # rooms: [Room!]!
+// # following: [User!]! @relation(name: "Follow")
+// # followers: [User!]! @relation(name: "Follow")
 
 export type CTX = {
 	request: express.Request;
