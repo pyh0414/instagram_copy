76a2e5bc594d2b4e210e9101ac072431551e2130
yeonho Park <whwlsvy12@naver.com>
2020-06-25 14:57:28 +0900

    add ignore test mode

--- server/src/middleware/authetication.ts
+++ server/src/middleware/authetication.ts
@@ -9,18 +9,27 @@ export const authetication = async (
 	info: any
 ) => {
 	try {
+		const isTest =
+			context.request.request.headers.authorization.split("Bearer ")[1] ||
+			false;
+
+		if (isTest) {
+			//테스트 모드인 경우는 pass
+			return;
+		}
+
 		const queryName: string = context.request.request.body.operationName || "";
 		if (!queryName) {
 			// 애초에 query name이 없으면 예외처리
 			throw new Error();
 		}
+
 		const shouldPassAuth: string[] = [
 			// 해당 query는 auth없이 그냥 패스
 			"_signIn",
 			"_signUpGetUser",
 			"_signUpSingleFileUpload",
 			"_signUpCreateUser",
-			"_test",
 			"_getAllPosts",
 		];
 
@@ -29,6 +38,9 @@ export const authetication = async (
 				"Bearer "
 			)[1];
 
+			if (token === "_test") {
+				return;
+			}
 			// 해당 query가 아니면 auth
 			const tokenValid: string = await jwt.verify(token, "secret");
 			if (!tokenValid) {
