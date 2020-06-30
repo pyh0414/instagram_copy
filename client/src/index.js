74433f610146100500436276003e092f14df5602
yeonho Park <whwlsvy12@naver.com>
2020-06-30 15:09:29 +0900

    remove initial header auth

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,6 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createUploadLink } from "apollo-upload-client";
 
-import userResolver from "./resolvers/user";
-import postResolver from "./resolvers/post";
-
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less' // antd를 사용하기 위함
 import App from "./App";
 
@@ -17,11 +14,7 @@ const client = new ApolloClient({
 	cache,
 	link: createUploadLink({
 		uri: "http://localhost:4000/graphql",
-		headers: {
-			authorization: `Bearer ${localStorage.getItem("token")}`,
-		},
 	}),
-	resolvers: [userResolver, postResolver],
 	connectToDevTools: true,
 });
 
