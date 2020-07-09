b0396d5ecb93b4d7dd0a5d2a8bbe31d3185ba0db
yeonho Park <whwlsvy12@naver.com>
2020-07-09 18:37:18 +0900

    remove validate_user

--- client/src/typeValidate.js
+++ client/src/typeValidate.js
@@ -11,25 +11,6 @@ const VALIDATE_ALL_POSTS = gql`
 	${fullPostInfo}
 `;
 
-const VALIDATE_USER = gql`
-	query {
-		user {
-			...user_info
-			following {
-				...follow_info
-			}
-			follower {
-				...follow_info
-			}
-			myPosts {
-				...full_post_info
-			}
-		}
-	}
-	${followInfo}
-	${fullPostInfo}
-`;
-
 const VALIDATE_LOGGED_IN_USER = gql`
 	query {
 		user {
@@ -48,9 +29,4 @@ const VALIDATE_OTHER_USER = gql`
 	${fullUserInfo}
 `;
 
-export {
-	VALIDATE_ALL_POSTS,
-	VALIDATE_USER,
-	VALIDATE_LOGGED_IN_USER,
-	VALIDATE_OTHER_USER,
-};
+export { VALIDATE_ALL_POSTS, VALIDATE_LOGGED_IN_USER, VALIDATE_OTHER_USER };
