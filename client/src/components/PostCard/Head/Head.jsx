5e4e5c5599daea5f0dd19ad4ac6537dbadde09d5
yeonho Park <whwlsvy12@naver.com>
2020-07-14 23:14:57 +0900

    add user click

--- client/src/components/PostCard/Head/Head.jsx
+++ client/src/components/PostCard/Head/Head.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { useApolloClient, useLazyQuery } from "@apollo/react-hooks";
+import { navigate } from "@reach/router";
 
 import { Wrapper, ImgCustom, UserCustom } from "./style";
+import { QUERY_OTHER_USER } from "../../../action/query";
+import { VALIDATE_OTHER_USER } from "../../../typeValidate";
 
 const Head = ({ author }) => {
+	const client = useApolloClient();
+
+	const [searchUser] = useLazyQuery(QUERY_OTHER_USER, {
+		onCompleted: ({ searchUsers }) => {
+			const otherUser = searchUsers[0];
+			client.writeQuery({
+				query: VALIDATE_OTHER_USER,
+				data: { otherUser },
+			});
+			navigate("/user");
+		},
+	});
+
+	const onItemClick = () => {
+		const userId = author.userId;
+		searchUser({
+			variables: { userId },
+			context: {
+				headers: {
+					authorization: `Bearer ${localStorage.getItem("token")}`,
+				},
+			},
+		});
+	};
+
 	return (
-		<Wrapper>
+		<Wrapper onClick={onItemClick} style={{ cursor: "pointer" }}>
 			{<ImgCustom src={`http://localhost:4000/${author.profile}`} />}
 			{<UserCustom>{author.userId}</UserCustom>}
 		</Wrapper>
