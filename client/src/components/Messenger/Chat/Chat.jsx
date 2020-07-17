259496ccc7e215f7c37ee6332b35b4ec2666beb5
yeonho Park <whwlsvy12@naver.com>
2020-07-17 16:34:27 +0900

    remove loading

--- client/src/components/Messenger/Chat/Chat.jsx
+++ client/src/components/Messenger/Chat/Chat.jsx
@@ -6,7 +6,7 @@ import Foot from "./Foot";
 import { CLIENT_LOGGED_IN_USER } from "../../../action/client";
 
 const Chat = ({ allRooms, onLeaveRoom, currentRoomId }) => {
-	const { data, loading } = useQuery(CLIENT_LOGGED_IN_USER);
+	const { data } = useQuery(CLIENT_LOGGED_IN_USER);
 	const currentRoom = allRooms.find((v) => v.id === currentRoomId);
 	return (
 		<>
