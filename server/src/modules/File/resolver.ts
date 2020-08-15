d1c7599e914fda76172f8391957d56abf27b462b
pyh0414 <whwlsvy12@naver.com>
2020-08-15 19:11:54 +0900

    modify

--- server/src/modules/File/resolver.ts
+++ server/src/modules/File/resolver.ts
@@ -1,57 +1,59 @@
-import { Resolver, Mutation, Arg, Query } from "type-graphql";
-import { GraphQLUpload } from "graphql-upload";
+import { Resolver, Mutation, Arg, Query, Authorized } from "type-graphql";
+import { GraphQLUpload } from "../../utils/graphqlUpload";
+import AWS from "aws-sdk";
+
 import { SINGLE_FILE_UPLOAD } from "./type";
 
 import uploadFile from "../../utils/uploadFile";
 import removeFile from "../../utils/removeFile";
 import { s3UploadFile } from "../../utils/s3UploadFile";
-
-import AWS from "aws-sdk";
-const uuid = require("uuid/v4");
+import { link } from "fs";
 
 @Resolver()
 export class FileResolver {
-	@Query((returns) => String, { nullable: true })
-	async fileRemove(
-		@Arg("src")
-		src: string
-	) {
-		try {
-			const result: boolean = await removeFile(src);
-			return result ? src : "";
-		} catch (err) {
-			throw new Error(err);
-		}
-	}
+  @Authorized()
+  @Query((returns) => String, { nullable: true })
+  async fileRemove(
+    @Arg("src")
+    src: string
+  ) {
+    try {
+      const result: boolean = await removeFile(src);
+      return result ? src : "";
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 
-	@Mutation((returns) => [String]!, { nullable: true })
-	async singleFileUpload(
-		@Arg("file", () => GraphQLUpload!)
-		file: SINGLE_FILE_UPLOAD
-	) {
-		try {
-			await uploadFile(file);
-			const filePath = [];
-			filePath.push(`images/${file.filename}`);
-			return filePath;
-		} catch (err) {
-			console.log(err);
-		}
-	}
+  @Mutation((returns) => [String]!, { nullable: false })
+  async singleFileUpload(
+    @Arg("file", () => GraphQLUpload)
+    file: SINGLE_FILE_UPLOAD
+  ) {
+    try {
+      const filePath = await uploadFile(file);
+      const result = [];
+      result.push(filePath);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-	@Mutation((returns) => [String]!, { nullable: false })
-	async multipleFileUpload(
-		@Arg("files", () => [GraphQLUpload!]!)
-		files: Array<[string, any]>
-	) {
-		try {
-			const filePaths: Array<string> = await Promise.all(
-				files.map((v) => uploadFile(v))
-			);
-			return filePaths;
-		} catch (err) {
-			console.log(err);
-			throw new Error(err);
-		}
-	}
+  @Authorized()
+  @Mutation((returns) => [String]!, { nullable: false })
+  async multipleFileUpload(
+    @Arg("files", () => [GraphQLUpload!]!)
+    files: Array<[string, any]>
+  ) {
+    try {
+      const result: Array<string> = await Promise.all(
+        files.map((v) => uploadFile(v))
+      );
+      return result;
+    } catch (err) {
+      console.log(err);
+      throw new Error(err);
+    }
+  }
 }
