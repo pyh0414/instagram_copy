486b2f9c98d993c1eb506110984b3686219e6289
pyh0414 <whwlsvy12@naver.com>
2020-11-24 21:24:57 +0900

    add user resolver test

--- server/src/test/resolver.user.ts
+++ server/src/test/resolver.user.ts
@@ -1,17 +1,36 @@
 import faker from "faker";
 import { PrismaClient } from "@prisma/client";
-import { request } from "graphql-request";
+import { GraphQLClient } from "graphql-request";
+
+import getHashedPassword from "../utils/getHashedPassword";
 
 const prisma = new PrismaClient();
 
-const createUser = `
-mutation($user: createUserInput!) {
-    createUser(user: $user)
-}
-`;
+const client = new GraphQLClient("http://localhost:4000/graphql", {
+  headers: {
+    authorization: "test",
+  },
+});
 
-const getUser = `
-query($userId: String!) {
+const testUser1 = {
+  userId: "test1UserId",
+  userPw: "test1UserPw",
+  name: "test1Name",
+  profile: "test1Profile",
+  refreshToken: "test1RefreshToken",
+};
+
+const testUser2 = {
+  userId: "test2UserId",
+  userPw: "test2Password",
+  name: "test2Name",
+  profile: "test2Profile",
+  refreshToken: "test2RefreshToken",
+};
+
+// 아이디 중복체크
+const userDuplicateCheck = ` 
+query _userDuplicateCheckTest($userId: String!) {
 	user(userId: $userId) {
 		userId
 		name
@@ -19,64 +38,225 @@ query($userId: String!) {
 }
 `;
 
-const testUser = {
-	userId: "testUserId",
-	userPw: "testPassword",
-	name: "testName",
-	profile: "testProfile",
-};
+// 로그인
+const signIn = ` 
+query _signInTest($user: signInInput!) {
+	signIn(user: $user) {
+    user{
+      userId
+      userPw
+    }
+    accessToken
+    loginResult
+	}
+}
+`;
 
-const endPoint = "http://localhost:4000/graphql";
+// 유저 검색
+const searchUser = ` 
+query _searchUserTest($userId: String!) {
+  searchUsers(userId: $userId){
+    userId
+  }
+}
+`;
+
+// 사용자 생성
+const createUser = ` 
+	mutation _createUserTest($user: createUserInput!) {
+		createUser(user: $user)
+	}
+`;
+
+// 팔로우
+const follow = ` 
+	mutation _followTest($data: followUnfollowUserInput!) {
+		followUser(data: $data){
+      me{
+        following{
+          userId
+        }
+      }
+      you{
+        follower{
+          userId
+        }
+      }
+    }
+	}
+`;
+
+// 팔로우
+const unFollow = ` 
+	mutation _followTest($data: followUnfollowUserInput!) {
+		unFollowUser(data: $data){
+      me{
+        following{
+          userId
+        }
+      }
+      you{
+        follower{
+          userId
+        }
+      }
+    }
+	}
+`;
 
 beforeAll(async () => {
-	await prisma.user.create({
-		data: testUser,
-	});
+  const hashPassword1 = await getHashedPassword(testUser1.userPw);
+  const hashPassword2 = await getHashedPassword(testUser2.userPw);
+
+  await prisma.user.create({
+    data: {
+      userId: testUser1.userId,
+      userPw: hashPassword1.toString(),
+      name: testUser1.name,
+      profile: testUser1.profile,
+      refreshToken: testUser1.refreshToken,
+    },
+  });
+
+  await prisma.user.create({
+    data: {
+      userId: testUser2.userId,
+      userPw: hashPassword2.toString(),
+      name: testUser2.name,
+      profile: testUser2.profile,
+      refreshToken: testUser2.refreshToken,
+    },
+  });
 });
 
 afterAll(async () => {
-	await prisma.user.delete({
-		where: {
-			userId: testUser.userId,
-		},
-	});
+  await prisma.user.delete({
+    where: {
+      userId: testUser1.userId,
+    },
+  });
+  await prisma.user.delete({
+    where: {
+      userId: testUser2.userId,
+    },
+  });
 });
 
-describe("User Resolver Test", () => {
-	it("create user", async () => {
-		const user = {
-			userId: faker.random.uuid().slice(0, 10),
-			userPw: faker.internet.password(),
-			name: faker.name.firstName() + faker.name.lastName(),
-			profile: faker.image.imageUrl(),
-		};
+describe("User Resolver ", () => {
+  test("사용자 생성", async () => {
+    const user = {
+      userId: faker.random.uuid().slice(0, 10),
+      userPw: faker.internet.password(),
+      name: faker.name.firstName() + faker.name.lastName(),
+      profile: faker.image.imageUrl(),
+    };
+    const res = await client.request(createUser, {
+      user,
+    });
+    expect(res).toMatchObject({
+      createUser: true,
+    });
+  });
 
-		try {
-			const res = await request(endPoint, createUser, {
-				user,
-			});
-			expect(res).toMatchObject({
-				createUser: true,
-			});
-		} catch (err) {
-			console.log("유저가 생성되지 않았습니다.");
-		}
-	});
+  test("아이디 중복확인", async () => {
+    const res = await client.request(userDuplicateCheck, {
+      userId: testUser1.userId,
+    });
+    expect(res["user"].userId).toBe(testUser1.userId);
+  });
 
-	it("get user", async () => {
-		try {
-			const res = await request(endPoint, getUser, {
-				userId: testUser.userId,
-			});
+  test("로그인", async () => {
+    const res = await client.request(signIn, {
+      user: {
+        userId: testUser1.userId,
+        userPw: testUser1.userPw,
+      },
+    });
+    expect(res["signIn"].loginResult).toBe(true);
+  });
 
-			expect(res).toMatchObject({
-				user: {
-					userId: testUser.userId,
-					name: testUser.name,
-				},
-			});
-		} catch (err) {
-			console.log("해당 id를 가진 유저가 존재하지 않습니다.");
-		}
-	});
+  test("유저 검색", async () => {
+    const res = await client.request(searchUser, {
+      userId: "test",
+    });
+    expect(res["searchUsers"].length).toBeGreaterThan(1);
+  });
+
+  test("팔로우", async () => {
+    const user1 = await prisma.user.findOne({
+      where: {
+        userId: testUser1.userId,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const user2 = await prisma.user.findOne({
+      where: {
+        userId: testUser2.userId,
+      },
+      select: {
+        id: true,
+      },
+    });
+    // user1(me)가 user2(you)를 follow 한다
+    // user1는 user2를 following
+    // user1는 user1의 follower
+    const res = await client.request(follow, {
+      data: {
+        me: user1.id,
+        you: user2.id,
+      },
+    });
+
+    const followerExpected = []; // user2의 follower에는 user1이 있어야함
+    followerExpected.push(res["followUser"].you.follower[0].userId);
+
+    const followingExpected = []; // user1의 following에는 user2가 있어야함
+    followingExpected.push(res["followUser"].me.following[0].userId);
+
+    expect(followerExpected).toEqual(
+      expect.arrayContaining([testUser1.userId])
+    );
+    expect(followingExpected).toEqual(
+      expect.arrayContaining([testUser2.userId])
+    );
+  });
+
+  test("언팔로우", async () => {
+    const user1 = await prisma.user.findOne({
+      where: {
+        userId: testUser1.userId,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const user2 = await prisma.user.findOne({
+      where: {
+        userId: testUser2.userId,
+      },
+      select: {
+        id: true,
+      },
+    });
+    const res = await client.request(unFollow, {
+      data: {
+        me: user1.id,
+        you: user2.id,
+      },
+    });
+
+    const followerExpected = res["unFollowUser"].you.follower;
+    const followingExpected = res["unFollowUser"].me.following;
+
+    expect(followerExpected).toEqual(
+      expect.not.arrayContaining([testUser1.userId])
+    );
+    expect(followingExpected).toEqual(
+      expect.not.arrayContaining([testUser2.userId])
+    );
+  });
 });
