1aad3abcee44d2d852fda5103582f0683a4d9c9f
pyh0414 <whwlsvy12@naver.com>
2020-11-24 16:29:04 +0900

    remove unused module

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,16 @@ import { Router } from "@reach/router";
 import { useQuery } from "@apollo/react-hooks";
 import { useApolloClient } from "@apollo/react-hooks";
 
-import Pages from "./pages";
 import SignIn from "./pages/signIn";
 import SignUp from "./pages/signUp";
 import Main from "./pages/main";
 import User from "./pages/user";
-import Header from "./components/Header";
 
 import { QUERY_ALL_POSTS, QUERY_ALL_ROOMS } from "./action/query";
 import { VALIDATE_ALL_POSTS, VALIDATE_ALL_ROOMS } from "./typeValidate";
-import { IS_LOGGED_IN } from "./action/client";
 
 const App = () => {
   const client = useApolloClient();
-  const { data } = useQuery(IS_LOGGED_IN);
 
   useQuery(QUERY_ALL_POSTS, {
     onCompleted: (data) => {
